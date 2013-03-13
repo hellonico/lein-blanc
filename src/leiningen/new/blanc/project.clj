76448a7a6da29319beb7ccc3a51ad000ef6d149a
Nicolas Modrzyk <nico@fivecool.net>
2013-03-13 13:51:16 +0900

    route samples

--- src/leiningen/new/blanc/project.clj
+++ src/leiningen/new/blanc/project.clj
@@ -4,10 +4,10 @@
             :dependencies [[org.clojure/clojure "1.5.0-RC3"]
             			   [me.raynes/laser "1.1.1"]
             			   [ring-cors/ring-cors "0.1.0"]
-                           [ring "1.1.8"]
-                           [lib-noir "0.4.9"]
+                        [ring "1.2.0-beta1"]
+                        [lib-noir "0.4.9"]
                  		   [compojure "1.1.5"]
-                           ]
+                         ]
 			:ring {:handler {{name}}.server/handler
          	:init    {{name}}.server/init
             :destroy {{name}}.server/destroy}
--- src/leiningen/new/blanc/server.clj
+++ src/leiningen/new/blanc/server.clj
@@ -1,14 +1,18 @@
 (ns {{name}}.server
   (:use compojure.core)
+  (:require [noir.response :as resp])
   (:require [noir.util.middleware :as middleware])
   (:require [compojure.route :as route]))
 
 (defroutes app
   (GET "/" [] "<h1>Hello World!</h1>")
+  (GET "/json/:id" [id] (resp/json {:name id}))
+  (GET "/headers" [] {:status 200 :headers {"Content-Type" "text/html"} :body "<h1>Hello with headers</h1>"})
+  (POST "/post" {params :body} (prn "params:" (slurp params)))
   (route/not-found "<h1>Page not found</h1>"))
 
 (defn init[]
-  (println "{{name}} started successfully..."))
+  (println "test3 started successfully..."))
 
 (defn destroy[]
   (println "shutting down..."))
