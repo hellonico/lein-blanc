e772629f6362f34cf16361ffd1b99b082aaa5f7d
Nicolas Modrzyk <hellonico@gmail.com>
2013-03-22 09:15:27 +0900

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject blanc/lein-template "1.1.1"
   :description "A lein-newnew template for generating new clojure web projects."
-  :dependencies [[org.clojure/clojure "1.5.0-RC3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :url "https://github.com/hellonico/lein-noir")
--- src/leiningen/new/blanc/core/project.clj
+++ src/leiningen/new/blanc/core/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :plugins [[lein-ring "0.8.3"]]
-            :dependencies [[org.clojure/clojure "1.5.0-RC3"]
+            :dependencies [[org.clojure/clojure "1.5.1"]
             			   [me.raynes/laser "1.1.1"]
             			   [ring-cors/ring-cors "0.1.0"]
                         [ring "1.2.0-beta1"]
