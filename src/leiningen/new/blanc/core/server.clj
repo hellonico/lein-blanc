f0eb2b5d100eda4f83ea140652f0997980a25e50
Nicolas Modrzyk <hellonico@gmail.com>
2013-03-24 23:16:47 +0900

    more bunches

--- src/leiningen/new/blanc/core/server.clj
+++ src/leiningen/new/blanc/core/server.clj
@@ -1,13 +1,14 @@
 (ns {{name}}.server
   (:use compojure.core)
   (:use [ring.middleware.resource])
+  (:use [ring.adapter.jetty :only [run-jetty]])
   (:require [clabango.parser :refer [render-file]])
   (:require [noir.response :as resp])
   (:require [noir.util.middleware :as middleware])
   (:require [compojure.route :as route]))
 
 (defroutes app
-  (GET "/" [] "<h1>Hello World!</h1>")
+  (GET "/" [] (render-file "{{name}}/html/main.html" {}))
   (GET "/json/:id" [id] (resp/json {:name id}))
   (GET "/headers" [] {:status 200 :headers {"Content-Type" "text/html"} :body "<h1>Hello with headers</h1>"})
   (POST "/post" {params :body} (prn "params:" (slurp params)))
